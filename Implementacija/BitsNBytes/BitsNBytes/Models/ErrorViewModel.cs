5a89019944f6c8365797428c2e3439e906684de7
izdralovic1 <44225892+izdralovic1@users.noreply.github.com>
2022-06-15 19:53:45 +0200

    Implementacija modela aplikacije

---
+++ Implementacija/BitsNBytes/BitsNBytes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BitsNBytes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Implementacija/BitsNBytes/BitsNBytes/Models/SpašeniArtikal.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BitsNBytes.Models
+{
+    public class SpašeniArtikal
+    {
+       [Key]
+       [ForeignKey("Artikal")]
+       public int Id { get; set; }
+
+        [ForeignKey("Korisnik")]
+        public int IdKorisnik { get; set; }
+
+    }
+}
